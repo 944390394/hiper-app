d4d46b6ad02e856bfbd2e2bbddd2ac74f9f94db8
944390394 <85352655+944390394@users.noreply.github.com>
2022-07-08 23:17:38 +0800

    更新

---
+++ source code/chatfree.bat
@@ -0,0 +1,32 @@
+mode con cols=50 lines=30
+@echo off
+set path==%path%;%appdata%\hiper
+title "hiper聊天程序免费版"
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+echo 当前运行路径是：%CD%
+echo 已获取管理员权限
+set /a n+=1
+echo *******************************
+echo **欢迎使用hiper聊天程序免费版**
+echo *******************************
+echo *     脚本作者：梦游泪世      *
+echo *     凭证 邀请码  有售       *
+echo *      QQ：944390394          *
+echo +++++++++++++++++++++++++++++++
+echo =       输入你的聊天昵称      =
+echo =       按下回车便可启用      =
+echo ————————————————
+set /p name=输入你的昵称：
+
+hiper c --names %name%
+echo.
+echo.
+echo [请检查是否已过30分钟，或检查是否有文件更新]
+pause
---
+++ source code/saterhiper.bat
@@ -0,0 +1,30 @@
+@echo off
+set path==%path%;%appdata%\hiper
+title "hiper启动程序"
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+echo 当前运行路径是：%CD%
+echo 已获取管理员权限
+echo **********************
+echo **欢迎使用hiper程序**
+echo **********************
+echo *脚本作者：梦游泪世  *
+echo *凭证 邀请码  有售   *
+echo * QQ：944390394      *
+echo ++++++++++++++++++++++
+echo =  把秘钥输入到下方  =
+echo =  按下回车便可启用  =
+echo =联机时请勿关闭此窗口=
+echo ———————————
+set /p mm=输入你的秘钥：
+hiper v -t %mm%
+echo.
+echo.
+echo [启动失败，请检查秘钥是否填写正确，或检查是否有文件更新]
+pause
